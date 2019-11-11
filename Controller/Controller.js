5908380a856399221bb578cfa5ee5f95106ec26f
Martin <martin-yde-jp@hotmail.dk>
2019-11-11 12:21:37 +0100

    US1 Controller metoder samt oprettelse af app connections

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,1 +1,19 @@
- 
+ const User = require('../Model/User');
+
+exports.createUser = function (name, rights, position, password) {
+    const user = new User({
+        name: name,
+        rights: rights,
+        position: position,
+        password: password
+    });
+    return user.save();
+};
+
+exports.getUser = function (userId) {
+    return User.findOne({_id: userId}).exec;
+};
+
+exports.getUsers = function () {
+    return User.find().populate().exec();
+}
