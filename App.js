295f789e97d59e532d6b615665ebb1998cf0d393
Martin <martin-yde-jp@hotmail.dk>
2019-11-12 12:51:19 +0100

    Stuff

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-console.log("Test af Heroku forbindelse");
 // INITIALIZATION
 const express = require('express');
 const morgan = require('morgan');
+const config = require('./config');
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan('tiny'));
 
+// MONGODB & MONGOOSE SETUP
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb+srv://AsgerDinesen:ludersoen@cluster0-cokpw.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
 // ROUTES FOR THE APP
 const userRouter = require("./Storage/CloudHandling");
 app.use('/api/users', userRouter);
 
-// MONGODB & MONGOOSE SETUP
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
-mongoose.connect('mongodb+srv://AsgerDinesen:Dopamin69@cluster0-cokpw.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-
-/*
 // START THE SERVER
 const port = process.env.PORT || config.localPort;
 app.listen(port);
 console.log('Listening on port ' + port + ' ...');
 
 module.exports = app; // pga. test
- */
