43dcd699b30a328269a7a10fdfed5587c74c4a61
Benjamin Mønster <Benjamin.Moenster@gmail.com>
2019-11-13 11:57:51 +0100

    test breakthrough!!!

--- Test/UnitTestTask.js
+++ Test/UnitTestTask.js
@@ -1,45 +1,79 @@
-const Controller = require('../Controllers/Controller');
-const assert = require('assert'); 
+"use strict";
+const should = require('should');
 
-// Standardobjekt der skal virke med deadline i format type 1
-let task1 = Controller.createTask('Vindues Polering', 'Beskidte vinduer hele blokken', '1995-12-17T03:24:00');
+const mocha = require('mocha');
+const { Task } = require("../Model/Task");
+let Controller = require("../Controllers/Controller");
 
-// Standardobjekt der skal virke med deadline i format type 2
-let task2 = Controller.createTask('Utætte rør', 'Utætte rør i underetagen st tv', 'December 17, 1995 03:24:00');
+mocha.setup;
 
-// Objekt uden deadline
-let task3 = Controller.createTask('Revner i væggen i opgangen', 'Se på og vurder skader');
+describe('unitTest', () => {
+    it("StandardObjekt med deadline i format 1", () => {
+        let task1 = [{
+            "name": "Vindues Polering",
+            "description": "Beskidte vinduer hele blokken",
+            "deadline": "1995-12-17T03:24:00"
+        }];
+        task1[0].name.should.be.equal('Vindues Polering')
+    });
+    it("StandardObjekt med deadline i format 2", () => {
+        let task2 = [{
+            "name": "Utætte rør",
+            "description": "Utætte rør i underetagen st tv",
+            "deadline": "December 17, 1995 03:24:00'00"
+        }]
+    })
+    it("Objekt uden deadline", () => {
+        let task3 = [{
+            "name": "Revner i væggen i opgangen",
+            "description": "Se på og vurder skader",
+            "deadline": null
+        }]
+    })
+    it("Objekt med deadline i forkert format", () => {
+        let task4 = [{
+            "name": "Flækket toilet",
+            "description": "Udskift toilettet i stuen th",
+            "deadline": "15 - 09 - 20 - 15 - 00"
+        }]
+    });
 
-// Objekt med deadline i forkert format
-let task4 = Controller.createTask('Flækket toilet', 'Udskift toilettet i stuen th', 15-09-20-15-00);
+})
 
-// Getting all the Tasks from the database
-let allTasks = Controller.getTasks();
+// describe('unitTest', () => {
+//     it('genererUserTabel unden users', () => {
+//         genererUserTabel([/* tom */]).should.be.equal('<table></table>');
+//     });
 
-// Testing task 1
-let testTask1 = Controller.getTask('Vindues Polering');
-assert.equal(testTask1.name , 'Vindues Polering');
-assert.equal(testTask1.description , 'beskidte vinduer');
-assert.equal(testTask1.deadline , '1995-12-17T03:24:00');
-assert.equal(testTask1.status, 'OPEN');
-
-// Testing task 2
-let testTask2 = Controller.getTask('Utætte rør');
-assert.equal(testTask2.name , 'Utætte rør');
-assert.equal(testTask2.description , 'Utætte rør i underetagen st tv');
-assert.equal(testTask2.deadline , 'December 17, 1995 03:24:00');
-assert.equal(testTask2.status, 'OPEN');
-
-// Testing task 3
-let testTask3 = Controller.getTask('Revner i væggen i opgangen');
-assert.equal(testTask3.name , 'Revner i væggen i opgangen');
-assert.equal(testTask3.description , 'Se på og vurder skader');
-assert.equal(testTask3.deadline , null);
-assert.equal(testTask3.status, 'OPEN');
-
-// Testing task 4
-let testTask4 = Controller.getTask('Flækket toilet');
-assert.equal(testTask4.name , 'Flækket toilet');
-assert.equal(testTask4.description , 'Udskift toilettet i stuen th');
-assert.notEqual(testTask4.deadline , 15-09-20-15-00);
-assert.equal(testTask4.status, 'OPEN');
+//     it('genererUserTabel med én user', () => {
+//         let enUser = [
+//             {
+//                 "id": 1,
+//                 "name": "Leanne Graham",
+//                 "username": "Bret",
+//                 "email": "Sincere@april.biz",
+//                 "address": {
+//                     "street": "Kulas Light",
+//                     "suite": "Apt. 556",
+//                     "city": "Gwenborough",
+//                     "zipcode": "92998-3874",
+//                     "geo": {
+//                         "lat": "-37.3159",
+//                         "lng": "81.1496"
+//                     }
+//                 },
+//                 "phone": "1-770-736-8031 x56442",
+//                 "website": "hildegard.org",
+//                 "company": {
+//                     "name": "Romaguera-Crona",
+//                     "catchPhrase": "Multi-layered client-server neural-net",
+//                     "bs": "harness real-time e-markets"
+//                 }
+//             }
+//         ];
+//         let enUserTabel =
+//             `<table> <tr><td>1</td><td>Leanne Graham</td><td>Romaguera-Crona</td></tr>\n</table>`;
+//         genererUserTabel(enUser).should.be.equal(enUserTabel);
+//         genererUserTabel(enUser).should.be.
+//     });
+// });
