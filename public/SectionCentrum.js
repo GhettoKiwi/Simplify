8a9f344c8cd80cfc0ba1704a898e18e131636293
Asger Dinesen <asgerboye121@gmail.com>
2019-11-18 16:22:13 +0100

    veeeeeeeeeeeeeeeee

--- public/SectionCentrum.js
+++ public/SectionCentrum.js
@@ -19,7 +19,7 @@ async function generateTaskTable(task) {
 }
 
 async function getTask(task) {
-    taskId = task.querySelector("#id");
+    taskId = task.querySelector("#id").innerHTML;
     console.log(taskId);
     let taskDB = await GET('/tasks/'+taskId);
     nameField.innerHTML = taskDB.name;
