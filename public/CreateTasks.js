8bcdff949c87713e77fba3020ecd750eff9822fd
Asger Dinesen <asgerboye121@gmail.com>
2019-11-18 12:56:57 +0100

    ggg

--- public/CreateTasks.js
+++ public/CreateTasks.js
@@ -18,20 +18,6 @@ async function POST(url, data) {
     return await response.json();
 }
 
-async function GETtext(url) {
-    const OK = 200;
-    let response = await fetch(url);
-    if (response.status !== OK)
-        throw new Error("GET status code " + response.status);
-    return await response.text();
-}
-
-async function generateTaskTable(task) {
-    let template = await GETtext('/task.hbs');
-    let compiledTemplate = Handlebars.compile(template);
-    return compiledTemplate({ task });
-}
-
 async function CreateTasks() {
     let name = document.getElementById('Name').value;
     let description = document.getElementById('Description').value;
---
+++ public/SectionCentrum.js
@@ -0,0 +1,22 @@
+async function GETtext(url) {
+    const OK = 200;
+    let response = await fetch(url);
+    if (response.status !== OK)
+        throw new Error("GET status code " + response.status);
+    return await response.text();
+}
+
+async function generateTaskTable(task) {
+    let template = await GETtext('/task.hbs');
+    let compiledTemplate = Handlebars.compile(template);
+    return compiledTemplate({ task });
+}
+
+async function main() {
+
+}
+
+async function update() {
+    let taskTable = document.getElementById('OverviewOverListViewCentrum');
+    taskTable.innerHTML = generateTaskTable();
+}
